e70a7e4f7b28f61f3b8ede6c68d98476d061c030
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2024-10-08 19:58:27 +0530

    commit

---
+++ 800/halloumiboxes.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        vector<int> A;
+        for(int i = 0; i < n; i++){
+            int num;
+            cin >> num;
+            A.push_back(num);
+        }
+        if(is_sorted(A.begin(), A.end())) cout << "YES" << endl;
+        else{
+            if(A.size() == 1) cout << "NO" << endl;
+            else if(k == 1) cout << "NO" << endl;
+            else cout << "YES" << endl;
+        }
+    }
+    return 0; 
+}
