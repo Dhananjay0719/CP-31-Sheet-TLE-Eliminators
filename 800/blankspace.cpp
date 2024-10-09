71049f6085fb5b1246ce4e125256731aedd85ab3
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2024-10-09 22:40:16 +0530

    commit

---
+++ 800/blankspace.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, num, count = 0, max_len = 0;
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> num;
+            if(num == 0) count++;
+            else count = 0;
+            max_len = max(max_len, count);
+        }
+        cout << max_len << endl;
+    }
+    return 0; 
+}
