22c11ed32ee45863a03bbe05c55c0615cad0147b
Dhananjay Badoni <101263267+Dhananjay0719@users.noreply.github.com>
2024-10-13 21:05:55 +0530

    commiy

---
+++ 800/arraycoloring.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, num, sum = 0;
+        cin >> n;
+        for(int i = 0; i < n; i++){
+            cin >> num;
+            sum += num;
+        }
+        if(sum % 2) cout << "No" << endl;
+        else cout << "Yes" << endl;
+    }
+    return 0; 
+}
